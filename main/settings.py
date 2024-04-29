2856e7bb95f0838189b11dac23b13c532b83f23e
Simon Boylan <siboy73@gmail.com>
2024-04-29 07:27:24 +0100

    Add home app and tempates

--- main/settings.py
+++ main/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+
+    # my apps
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -66,7 +69,10 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
