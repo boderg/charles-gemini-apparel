c3d99f71be79480dc14fb356589ac44fa13cc91e
Simon Boylan <siboy73@gmail.com>
2024-05-09 18:33:41 +0100

    Add categories and products to models

--- apparel/models.py
+++ apparel/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+# Categories model
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    friendly_name = models.CharField(max_length=250, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+# Products model
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(default=29.99, max_digits=6, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
