fb4163d2bcb472728b6ecdea6051e4c58adf02ea
Simon Boylan <siboy73@gmail.com>
2024-05-09 19:06:35 +0100

    Display products on new designs page

--- apparel/views.py
+++ apparel/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def apparel(request):
     """A view to display the products pages """
+    products = Product.objects.all()
 
-    return render(request, 'apparel/apparel.html')
+    return render(request, 'apparel/apparel.html', {'products': products})
