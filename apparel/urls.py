e58efabc360e4587db5258a54db9d0ebed875254
Simon Boylan <siboy73@gmail.com>
2024-05-11 21:56:09 +0100

    Add single product view

--- apparel/urls.py
+++ apparel/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('new/', views.new, name='new'),
     path('tees/', views.tees, name='tees'),
     path('hoodies/', views.hoodies, name='hoodies'),
+    path('garment/<product_id>/', views.garment, name='garment'),
 ]
--- apparel/views.py
+++ apparel/views.py
@@ -1,26 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
 def new(request):
-    """A view to display the products pages """
+    """A view to display the new designs page """
     products = Product.objects.all()
     page_title = 'New Designs'
 
-    return render(request, 'apparel/new.html', {'products': products, 'page_title': page_title})
+    return render(request, 'apparel/new.html',
+                  {'products': products, 'page_title': page_title})
 
 
 def tees(request):
-    """A view to display the products pages """
+    """A view to display the tees page """
     products = Product.objects.all()
     page_title = 'Tees'
 
-    return render(request, 'apparel/tees.html', {'products': products, 'page_title': page_title})
+    return render(request, 'apparel/tees.html',
+                  {'products': products, 'page_title': page_title})
 
 
 def hoodies(request):
-    """A view to display the products pages """
+    """A view to display the hoodies page """
     products = Product.objects.all()
     page_title = 'Hoodies'
 
-    return render(request, 'apparel/hoodies.html', {'products': products, 'page_title': page_title})
+    return render(request, 'apparel/hoodies.html',
+                  {'products': products, 'page_title': page_title})
+
+
+def garment(request, product_id):
+    """A view to display the garment page """
+    product = get_object_or_404(Product, pk=product_id)
+
+    return render(request, 'apparel/garment.html',
+                  {'product': product, 'page_title': product.name})
