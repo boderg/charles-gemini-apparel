1d77bafe3e82d6f39af8e56206a61133fb8a6c24
Simon Boylan <siboy73@gmail.com>
2024-05-13 11:24:37 +0100

    Add all garments view and template

--- apparel/urls.py
+++ apparel/urls.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('new/', views.new, name='new'),
-    path('tees/', views.tees, name='tees'),
-    path('hoodies/', views.hoodies, name='hoodies'),
+    # path('new/', views.new, name='new'),
+    # path('tees/', views.tees, name='tees'),
+    # path('hoodies/', views.hoodies, name='hoodies'),
     path('garment/<product_id>/', views.garment, name='garment'),
+    path('all_garments/', views.all_garments, name='all_garments'),
 ]
--- apparel/views.py
+++ apparel/views.py
@@ -2,36 +2,51 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
-def new(request):
-    """A view to display the new designs page """
-    products = Product.objects.all()
-    page_title = 'New Designs'
+# def new(request):
+#     """A view to display the new designs page """
+#     products = Product.objects.all()
+#     page_title = 'New Designs'
 
-    return render(request, 'apparel/new.html',
-                  {'products': products, 'page_title': page_title})
-
-
-def tees(request):
-    """A view to display the tees page """
-    products = Product.objects.all()
-    page_title = 'Tees'
-
-    return render(request, 'apparel/tees.html',
-                  {'products': products, 'page_title': page_title})
+#     return render(request, 'apparel/new.html',
+#                   {'products': products, 'page_title': page_title})
 
 
-def hoodies(request):
-    """A view to display the hoodies page """
-    products = Product.objects.all()
-    page_title = 'Hoodies'
+# def tees(request):
+#     """A view to display the tees page """
+#     products = Product.objects.all()
+#     page_title = 'Tees'
 
-    return render(request, 'apparel/hoodies.html',
-                  {'products': products, 'page_title': page_title})
+#     return render(request, 'apparel/tees.html',
+#                   {'products': products, 'page_title': page_title})
+
+
+# def hoodies(request):
+#     """A view to display the hoodies page """
+#     products = Product.objects.all()
+#     page_title = 'Hoodies'
+
+#     return render(request, 'apparel/hoodies.html',
+#                   {'products': products, 'page_title': page_title})
 
 
 def garment(request, product_id):
+
     """A view to display the garment page """
+
     product = get_object_or_404(Product, pk=product_id)
 
     return render(request, 'apparel/garment.html',
                   {'product': product, 'page_title': product.name})
+
+
+def all_garments(request):
+
+    """A view to display all garments, search queries and sorting """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'apparel/all_garments.html', context)
