bd23daa793d804ed626790beab111ceb1b24e1c2
Simon Boylan <siboy73@gmail.com>
2024-05-13 14:51:49 +0100

    Add search function

--- apparel/views.py
+++ apparel/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
@@ -34,10 +36,24 @@ def all_garments(request):
     """A view to display all garments, search queries and sorting """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('all_garments'))
+
+            queries = Q(
+                name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
         'page_title': 'All Designs',
+        'search_term': query,
     }
 
     return render(request, 'apparel/all_garments.html', context)
