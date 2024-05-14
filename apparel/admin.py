62c61aa031cf7c36972561689bd45333bc3b376d
Simon Boylan <siboy73@gmail.com>
2024-05-14 12:17:32 +0100

    Add colour and size models

--- apparel/admin.py
+++ apparel/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Colour, Size
 
 
 class ProductAdmin(admin.ModelAdmin):
 
     list_display = (
         'name',
-        'get_category',
+        'get_colours',
+        'get_sizes',
         'price',
         'discount',
+        'get_category',
         'description',
         'image',
         )
@@ -17,6 +19,16 @@ class ProductAdmin(admin.ModelAdmin):
         return ", ".join([category.name for category in obj.category.all()])
 
     get_category.short_description = 'Category'
+
+    def get_colours(self, obj):
+        return ", ".join([colour.name for colour in obj.colours.all()])
+
+    get_colours.short_description = 'Colours'
+
+    def get_sizes(self, obj):
+        return ", ".join([size.name for size in obj.sizes.all()])
+
+    get_sizes.short_description = 'Sizes'
 
     ordering = (
         'name',
@@ -33,3 +45,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Colour)
+admin.site.register(Size)
--- apparel/models.py
+++ apparel/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
 
+# Colour model
+class Colour(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+# Size model
+class Size(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 # Categories model
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -18,7 +34,9 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(default=0, max_digits=5, decimal_places=2)
-    category = models.ManyToManyField(Category)
+    category = models.ManyToManyField(Category, blank=True)
+    colours = models.ManyToManyField(Colour, blank=True)
+    sizes = models.ManyToManyField(Size, blank=True)
     image = models.ImageField(null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     # Discounts
