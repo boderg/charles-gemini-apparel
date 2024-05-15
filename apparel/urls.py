18d7588c18742f3847c7e02b9e3ba51f50bc1dc7
Simon Boylan <siboy73@gmail.com>
2024-05-15 16:02:08 +0100

    Change hoodie images

--- apparel/urls.py
+++ apparel/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
     path('garment/<product_id>/', views.garment, name='garment'),
     path('all_garments/', views.all_garments, name='all_garments'),
 ]
