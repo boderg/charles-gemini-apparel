a1e0f0f936cf7461bc430fb261296ee9c0b52413
Simon Boylan <siboy73@gmail.com>
2024-05-18 18:15:04 +0100

    Add bag_add functionality

--- bag/urls.py
+++ bag/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.bag, name='bag'),
-    path('add/', views.bag_add, name='bag_add'),
+    path('add/<item_id>/', views.bag_add, name='bag_add'),
     path('delete/', views.bag_delete, name='bag_delete'),
     path('update/', views.bag_update, name='bag_update'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def bag(request):
@@ -14,8 +14,27 @@ def bag(request):
     return render(request, 'bag/bag.html', context)
 
 
-def bag_add(request):
-    pass
+def bag_add(request, item_id):
+
+    """Add the specified item and quantity to the shopping bag"""
+
+    quantity = int(request.POST.get('quantity'))
+    colour = request.POST.get('colour')
+    size = request.POST.get('size')
+
+    bag = request.session.get('bag', {})
+
+    if item_id in bag:
+        bag[item_id]['quantity'] += quantity
+        if colour and size:
+            bag[item_id]['colour'] = colour
+            bag[item_id]['size'] = size
+    else:
+        bag[item_id] = {'quantity': 1, 'colour': colour, 'size': size}
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect('bag')
 
 
 def bag_delete(request):
