66085917e41347cd7bd9e099419d8b13a77d6646
Simon Boylan <siboy73@gmail.com>
2024-05-24 11:57:52 +0100

    Added toast templates and functionality

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .contexts import bag_contents
+from django.contrib import messages
+from apparel.models import Product
 
 
 def bag(request):
@@ -17,6 +19,8 @@ def bag_add(request, item_id):
 
     """Add the specified item and quantity to the shopping bag"""
 
+    product = Product.objects.get(pk=item_id)
+
     quantity = int(request.POST.get('quantity'))
     colour_id = request.POST.get('colour')
     size_id = request.POST.get('size')
@@ -32,6 +36,8 @@ def bag_add(request, item_id):
         bag[item_key] = {
             'item_id': item_id, 'quantity': quantity,
             'colour': colour_id, 'size': size_id}
+    messages.success(request, f'Added {bag[item_key]["quantity"]} x\
+            'f'{product.name} in your bag')
 
     request.session['bag'] = bag
     return redirect('bag')
@@ -40,6 +46,8 @@ def bag_add(request, item_id):
 def bag_update_or_delete(request, item_id):
 
     """Update or delete the specified item in the shopping bag"""
+
+    product = Product.objects.get(pk=item_id)
 
     action = request.POST.get('action')
     colour_id = request.POST.get('colour')
@@ -51,6 +59,7 @@ def bag_update_or_delete(request, item_id):
         item_key = f"{item_id}_{colour_id}_{size_id}"
         if item_key in bag:
             del bag[item_key]
+        messages.success(request, f'Removed {product.name} to your bag!')
     else:
         # Update the item
         quantity = int(request.POST.get('quantity'))
@@ -67,6 +76,8 @@ def bag_update_or_delete(request, item_id):
             'colour': colour_id,
             'size': size_id
         }
+        messages.success(request, f'Updated {bag[item_key]["quantity"]} x\
+            'f'{product.name} in your bag')
 
     request.session['bag'] = bag
     return redirect('bag')
