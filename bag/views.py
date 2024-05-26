659e7c929f86da2175f47114da49d7fcf95f7aa2
Simon Boylan <siboy73@gmail.com>
2024-05-26 18:24:45 +0100

    Add content to success toasts

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .contexts import bag_contents
 from django.contrib import messages
-from apparel.models import Product
+from apparel.models import Product, Colour, Size
 
 
 def bag(request):
@@ -19,7 +19,7 @@ def bag_add(request, item_id):
 
     """Add the specified item and quantity to the shopping bag"""
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
 
     quantity = int(request.POST.get('quantity'))
     colour_id = request.POST.get('colour')
@@ -36,8 +36,11 @@ def bag_add(request, item_id):
         bag[item_key] = {
             'item_id': item_id, 'quantity': quantity,
             'colour': colour_id, 'size': size_id}
-    messages.success(request, f'Added {bag[item_key]["quantity"]} x\
-            'f'{product.name} in your bag')
+        colour = Colour.objects.get(pk=colour_id)
+        size = Size.objects.get(pk=size_id)
+    messages.success(request, f'{bag[item_key]["quantity"]} x\
+            {product.name} in colour {colour.name} and size {size.name}\
+                has been added to your bag.')
 
     request.session['bag'] = bag
     return redirect('bag')
@@ -47,37 +50,50 @@ def bag_update_or_delete(request, item_id):
 
     """Update or delete the specified item in the shopping bag"""
 
-    product = Product.objects.get(pk=item_id)
+    try:
+        product = get_object_or_404(Product, pk=item_id)
 
-    action = request.POST.get('action')
-    colour_id = request.POST.get('colour')
-    size_id = request.POST.get('size')
-    bag = request.session.get('bag', {})
+        action = request.POST.get('action')
+        colour_id = request.POST.get('colour')
+        size_id = request.POST.get('size')
+        bag = request.session.get('bag', {})
 
-    if action == 'delete':
-        # Find and delete the item
-        item_key = f"{item_id}_{colour_id}_{size_id}"
-        if item_key in bag:
-            del bag[item_key]
-        messages.success(request, f'Removed {product.name} to your bag!')
-    else:
-        # Update the item
-        quantity = int(request.POST.get('quantity'))
-        # Remove the old item entry
-        for item_key in list(bag.keys()):
-            if item_key.startswith(f"{item_id}_"):
-                del bag[item_key]
-                break
-        # Create a new item key
-        item_key = f"{item_id}_{colour_id}_{size_id}"
-        bag[item_key] = {
-            'item_id': item_id,
-            'quantity': quantity,
-            'colour': colour_id,
-            'size': size_id
-        }
-        messages.success(request, f'Updated {bag[item_key]["quantity"]} x\
-            'f'{product.name} in your bag')
+        if action == 'delete':
+            try:
+                # Find and delete the item
+                item_key = f"{item_id}_{colour_id}_{size_id}"
+                if item_key in bag:
+                    del bag[item_key]
+                messages.success(request, f'{product.name}\
+                                 has been removed from your bag!')
+            except Exception as e:
+                messages.error(request, f'Error removing item: {e}')
+                return redirect('bag')
+        else:
+            # Update the item
+            quantity = int(request.POST.get('quantity'))
+            # Remove the old item entry
+            for item_key in list(bag.keys()):
+                if item_key.startswith(f"{item_id}_"):
+                    del bag[item_key]
+                    break
+            # Create a new item key
+            item_key = f"{item_id}_{colour_id}_{size_id}"
+            bag[item_key] = {
+                'item_id': item_id,
+                'quantity': quantity,
+                'colour': colour_id,
+                'size': size_id
+            }
+            colour = Colour.objects.get(pk=colour_id)
+            size = Size.objects.get(pk=size_id)
+            messages.success(request, f'{bag[item_key]["quantity"]} x\
+                {product.name} with colour {colour.name}\
+                    and size {size.name} has been updated in your bag.')
 
-    request.session['bag'] = bag
-    return redirect('bag')
+        request.session['bag'] = bag
+        return redirect('bag')
+
+    except Exception as e:
+        messages.error(request, f'Error updating your bag: {e}')
+        return redirect('bag')
