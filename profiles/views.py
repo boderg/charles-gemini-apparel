c097cbf1f93fd573d3761da9d06c9a6355e420d4
Simon Boylan <siboy73@gmail.com>
2024-06-04 15:48:09 +0100

    Connect profile page to link

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 def profile(request):
 
     """ A view to return the profile page """
 
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profile.html'
     context = {
         'page_title': 'My Profile',
+        'profile': profile
     }
 
     return render(request, template, context)
