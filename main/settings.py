3159786e5f79dfe7d33a04c96179cb677810f59e
Simon Boylan <siboy73@gmail.com>
2024-06-11 22:08:11 +0100

    Turn on debug to test deployed site

--- main/settings.py
+++ main/settings.py
@@ -29,7 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+
+DEBUG = True
 
 CSRF_TRUSTED_ORIGINS = [
     'https://charles-gemini-apparel-6a23d410a6ee.herokuapp.com']
