95728468786f85caca977a2cdfd9bac903a59a29
Simon Boylan <siboy73@gmail.com>
2024-06-11 22:54:40 +0100

    Turn off debug

--- main/settings.py
+++ main/settings.py
@@ -29,9 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 CSRF_TRUSTED_ORIGINS = [
     'https://charles-gemini-apparel-6a23d410a6ee.herokuapp.com']
