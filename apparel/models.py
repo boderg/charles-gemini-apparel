6d50645606663f9652a4bdb294dfaef654bba859
Simon Boylan <siboy73@gmail.com>
2024-06-15 22:16:57 +0100

    Remove unused images, migrate images model

--- apparel/models.py
+++ apparel/models.py
@@ -55,7 +55,7 @@ class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE)
     image = models.ImageField(
-        upload_to='product_images/')
+        upload_to='product_images/', blank=True, null=True)
 
     def __str__(self):
         return self.product.name
