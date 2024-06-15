bf050695734c22a883c84e07936c8a4c781a8bb0
Simon Boylan <siboy73@gmail.com>
2024-06-15 20:42:07 +0100

    Add carousel to main product image

--- apparel/models.py
+++ apparel/models.py
@@ -54,7 +54,8 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='product_images/')
+    image = models.ImageField(
+        upload_to='product_images/')
 
     def __str__(self):
         return self.product.name
