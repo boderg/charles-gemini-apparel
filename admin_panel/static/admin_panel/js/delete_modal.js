a8fda5ab43f896d19b346bbf87503efacea6d0c1
Simon Boylan <siboy73@gmail.com>
2024-06-18 21:01:01 +0100

    Correct errors

--- admin_panel/static/admin_panel/js/delete_modal.js
+++ admin_panel/static/admin_panel/js/delete_modal.js
@@ -1,10 +1,10 @@
 // delete modal that appears when delete buttons are clicked
 
 $('#deleteModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) 
-    var itemName = button.data('item-name') 
-    var deleteUrl = button.data('delete-url') 
-    var modal = $(this)
-    modal.find('.itemName').text(itemName)
-    modal.find('#deleteForm').attr('action', deleteUrl)
-  })
+    var button = $(event.relatedTarget); 
+    var itemName = button.data('item-name'); 
+    var deleteUrl = button.data('delete-url'); 
+    var modal = $(this);
+    modal.find('.itemName').text(itemName);
+    modal.find('#deleteForm').attr('action', deleteUrl);
+  });
